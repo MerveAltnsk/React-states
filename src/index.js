e295134f1c2829c078e9687cda3ca5eb1b49400b
merve <mervealtinisik99@gmail.com>
2024-12-19 19:51:05 +0300

    Input Example state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 // import App from './App';
-import Counter from './components/Counter';
+//import Counter from './components/Counter';
+import InputExample from './components/InputExample';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,8 +13,9 @@ root.render(
   <React.StrictMode>
 
     {/*  <App /> */}
+    {/*  <Counter /> */}
 
-    <Counter />
+    <InputExample />
 
   </React.StrictMode>
 );
